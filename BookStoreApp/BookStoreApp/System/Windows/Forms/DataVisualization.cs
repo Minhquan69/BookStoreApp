8ef285778ae2255c928952565eafb113483d36a1
MinhQuan <daomquan2004@gmail.com>
2025-04-29 15:44:57 +0700

    Add full admin interface

--- BookStoreApp/BookStoreApp/System/Windows/Forms/DataVisualization.cs
+++ BookStoreApp/BookStoreApp/System/Windows/Forms/DataVisualization.cs
@@ -1,4 +1,6 @@
-﻿namespace System.Windows.Forms
+﻿using System.Drawing;
+
+namespace System.Windows.Forms
 {
     internal class DataVisualization
     {
@@ -6,18 +8,33 @@
         {
             internal class Chart
             {
+                internal string Text;
+                internal int TabIndex;
+
+                public Size Size { get; internal set; }
+                public object Series { get; internal set; }
+                public string Name { get; internal set; }
+                public Point Location { get; internal set; }
+                public object Legends { get; internal set; }
+                public object ChartAreas { get; internal set; }
+                public bool Visible { get; internal set; }
             }
 
             internal class ChartArea
             {
+                public string Name { get; internal set; }
             }
 
             internal class Legend
             {
+                public string Name { get; internal set; }
             }
 
             internal class Series
             {
+                public string Name { get; internal set; }
+                public string Legend { get; internal set; }
+                public string ChartArea { get; internal set; }
             }
         }
     }
